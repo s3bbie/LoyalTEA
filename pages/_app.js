72098c919b03003d0181397346180c593c915977
sebbiechu <139472455+sebbiechu@users.noreply.github.com>
2025-08-19 11:24:51 +0000

    captcha fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,34 @@
-import Head from 'next/head';
-import '@/styles/globals.css';
-import '@/styles/login.css';
-import '@/styles/home.css';
-import '@/styles/rewards.css';
-import '@/styles/settings.css';
-import BottomNav from '../components/BottomNav';
-import { useRouter } from 'next/router';
-import { Analytics } from '@vercel/analytics/react';
+import "@/styles/globals.css";
+import "@/styles/login.css";
+import "@/styles/home.css";
+import "@/styles/rewards.css";
+import "@/styles/settings.css";
+import BottomNav from "../components/BottomNav";
+import { useRouter } from "next/router";
+import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
-  const hideNavOn = ['/', '/register', '/verify-email'];
-
+  const hideNavOn = ["/", "/register", "/verify-email"];
   const showNav = !hideNavOn.includes(router.pathname);
 
   return (
     <>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
+      <Script
+        id="recaptcha-v3"
+        src={`https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITEKEY}`}
+        strategy="afterInteractive"
+      />
+      <Script id="recaptcha-sitekey" strategy="afterInteractive">
+        {`window.RECAPTCHA_SITEKEY='${process.env.NEXT_PUBLIC_RECAPTCHA_SITEKEY}'`}
+      </Script>
 
       <Component {...pageProps} />
       {showNav && <BottomNav />}
-      
+
       <Analytics />
     </>
   );
 }
+
