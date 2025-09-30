2c37650cec4ce8b9cdb4ef4e71d65140015728ad
Seb <sebbiedesigns@gmail.com>
2025-09-30 12:41:11 +0000

    Testing Push Notification #1

--- pages/_document.js
+++ pages/_document.js
@@ -1,3 +1,4 @@
+// pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -13,6 +14,24 @@ export default function Document() {
           rel="stylesheet"
         />
         {isProd && <link rel="manifest" href="/manifest.json" />}
+
+        {/* ✅ OneSignal SDK */}
+        <script
+          src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js"
+          defer
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.OneSignalDeferred = window.OneSignalDeferred || [];
+              OneSignalDeferred.push(async function(OneSignal) {
+                await OneSignal.init({
+                  appId: "b09b5b16-054b-4326-87f3-74ae720ac14b",
+                });
+              });
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
